ec44e6c78080ac58b271ee7e666c7c8d80949cbc
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-27 17:10:01 -0600

    Foods can be added to meals

--- app/controllers/api/v1/meals/foods_controller.rb
+++ app/controllers/api/v1/meals/foods_controller.rb
@@ -16,5 +16,14 @@ class Api::V1::Meals::FoodsController < ApplicationController
     if meal.nil? || food.nil?
       render nothing: true, status: 404 and return
     end
+
+    meal.foods << food
+
+    if meal.save
+      msg = { message: "Successfully added #{food.name} to #{meal.name}" }
+      render json: msg, status: 201 and return
+    end
+
+    render nothing: true, status: 400
   end
 end
