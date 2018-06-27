01f42eb713997d352468b9d2094e030b28da1a9c
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-27 00:05:17 -0600

    Specific food can be requested

--- app/controllers/api/v1/foods_controller.rb
+++ app/controllers/api/v1/foods_controller.rb
@@ -2,4 +2,10 @@ class Api::V1::FoodsController < ApplicationController
   def index
     render json: Food.all
   end
+
+  def show
+    food = Food.find(params[:id])
+
+    render json: food
+  end
 end
