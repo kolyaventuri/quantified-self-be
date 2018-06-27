04c56add9dc08f9b0b3395d62fa22e084b1f7395
Kolya Venturi <kolya.venturi@enx3s.com>
2018-06-27 17:18:43 -0600

    Sad path handlers for deletion

--- app/controllers/api/v1/meals/foods_controller.rb
+++ app/controllers/api/v1/meals/foods_controller.rb
@@ -25,6 +25,12 @@ class Api::V1::Meals::FoodsController < ApplicationController
   end
 
   def destroy
+    meal = Meal.where(id: params[:meal_id]).first
+    food = Food.where(id: params[:id]).first
 
+
+    if meal.nil? || food.nil?
+      render nothing: true, status: 404 and return
+    end
   end
 end
